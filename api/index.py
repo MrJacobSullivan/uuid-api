2dabf9dd3d1758cd7bbf6aa9ba11b120d3f560f4
mrjacobsullivan <mrjacobsullivan@gmail.com>
2022-03-08 11:38:27 -0600

    added api endpoint

---
+++ api/index.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+from uuid import uuid4
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+
+        response = uuid4()
+        self.wfile.write(response.encode())
+        return
